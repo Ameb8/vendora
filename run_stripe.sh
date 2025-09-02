a92d2f99ad058fa5dc75d91a3cb8b64d7b9ca9f6
Alex Buckley <alexmeb81@outlook.com>
2025-09-02 01:34:16 -0700

    Updated stripe cli script to work with stripe connect webhook

--- run_stripe.sh
+++ run_stripe.sh
@@ -5,7 +5,7 @@ set -e  # exit on error
 # Log in to Stripe CLI
 stripe login --api-key "$STRIPE_PRIVATE"
 
-# Get the webhook secret (only needed once)
+# Get the webhook secret
 WEBHOOK_SECRET_FILE=./stripe/webhook.env
 
 if [ ! -f "$WEBHOOK_SECRET_FILE" ]; then
@@ -14,7 +14,9 @@ if [ ! -f "$WEBHOOK_SECRET_FILE" ]; then
   echo "Stripe webhook secret written to $WEBHOOK_SECRET_FILE"
 fi
 
-# Start listening and forwarding (keeps the container alive)
+# Start listening and forwarding
 stripe listen \
   --forward-to "http://host.docker.internal:8000/subscriptions/stripe/webhook/" \
-  --events customer.subscription.created,invoice.payment_failed
+  --events customer.subscription.created,invoice.payment_failed \
+  --forward-to "http://host.docker.internal:8000/payments/stripe/webhook/" \
+  --events payment_intent.succeeded,payment_intent.payment_failed
